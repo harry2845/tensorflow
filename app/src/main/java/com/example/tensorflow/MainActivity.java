55838fbc9d1120ba393ecdcf801e48c65d2820e7
Dong Shen <shdon@microsoft.com>
2020-01-16 17:23:50 +0800

    Change some code

--- app/src/main/java/com/example/tensorflow/MainActivity.java
+++ app/src/main/java/com/example/tensorflow/MainActivity.java
@@ -17,12 +17,9 @@ import org.json.JSONObject;
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.support.common.FileUtil;
-import org.tensorflow.lite.support.common.TensorProcessor;
-import org.tensorflow.lite.support.common.ops.NormalizeOp;
 import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.image.ops.ResizeOp;
-import org.tensorflow.lite.support.label.TensorLabel;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.BufferedReader;
@@ -36,7 +33,6 @@ import java.net.URL;
 import java.nio.MappedByteBuffer;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -45,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     Handler handler;
 
     private static final String KEY = "*******************";
+    private static final String PREDICT_URL = "*******************";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         }
         ImageView imageView = findViewById(R.id.image);
         imageView.setImageURI(uri);
-        Log.d("sdsdsd", "url: " + uri.toString());
+        Log.d(TAG, "url: " + uri.toString());
 
         MappedByteBuffer model = FileUtil.loadMappedFile(this, "model.tflite");
         Interpreter.Options options = new Interpreter.Options();
@@ -87,10 +85,10 @@ public class MainActivity extends AppCompatActivity {
         int[] outputShape = interpreter.getOutputTensor(0).shape();
         DataType outputDataType = interpreter.getOutputTensor(0).dataType();
 
-        Log.d("sdsdsd", "input shape: " + Arrays.toString(inputShape));
-        Log.d("sdsdsd", "input data type: " + inputDataType.name());
-        Log.d("sdsdsd", "output shape: " + Arrays.toString(outputShape));
-        Log.d("sdsdsd", "output data type: " + outputDataType.name());
+        Log.d(TAG, "input shape: " + Arrays.toString(inputShape));
+        Log.d(TAG, "input data type: " + inputDataType.name());
+        Log.d(TAG, "output shape: " + Arrays.toString(outputShape));
+        Log.d(TAG, "output data type: " + outputDataType.name());
 
         TensorImage inputImage = new TensorImage(inputDataType);
         Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
@@ -109,10 +107,10 @@ public class MainActivity extends AppCompatActivity {
         interpreter.run(inputImage.getBuffer(), outputBuffer.getBuffer());
 
         List<String> labels = FileUtil.loadLabels(this, "labels.txt");
-        Log.d("sdsdsd", "labels: " + Arrays.toString(labels.toArray()));
+        Log.d(TAG, "labels: " + Arrays.toString(labels.toArray()));
 
         float[] resultList = outputBuffer.getFloatArray();
-        Log.d("sdsdsd", "result: " + Arrays.toString(resultList));
+        Log.d(TAG, "result: " + Arrays.toString(resultList));
 
         int index = 0;
         float result = 0;
@@ -138,13 +136,13 @@ public class MainActivity extends AppCompatActivity {
 //
 //            for (String label : labels) {
 //                float result = floatMap.get(label);
-//                Log.d("sdsdsd", "label: " + label + "\t" + result);
+//                Log.d(TAG, "label: " + label + "\t" + result);
 //            }
 //        }
     }
 
     private void predictUrl(String text) {
-        Log.d("sdsdsd", "url: " + text);
+        Log.d(TAG, "url: " + text);
 
         loadImage(text);
         predict(text);
@@ -182,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    URL url = new URL("https://westus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/f8f373a4-86aa-4d33-b705-f9e59ea8ed31/classify/iterations/beartest/url");
+                    URL url = new URL(PREDICT_URL);
 
                     HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
@@ -208,7 +206,7 @@ public class MainActivity extends AppCompatActivity {
                             response.append("\r");
                         }
                         br.close();
-                        Log.d("sdsdsd", response.toString());
+                        Log.d(TAG, response.toString());
                         JSONObject jsonObject = new JSONObject(response.toString());
                         JSONArray resultArray = jsonObject.getJSONArray("predictions");
                         double result = 0;
